b0439cccea263ccd0c4e9c3213072f99f98a4dc6
Matt Asper <matt.asper@utexas.edu>
2022-01-28 16:19:39 -0600

    IQ updates
    most-recent updates

---
+++ Loads Data Processing/Tripod Stand Loads Processing/parkClarke.m
@@ -0,0 +1,19 @@
+function [Q,D] = parkClarke(A,B,C)
+%This function calculates the quadrature (Q) current from A,
+%B, and C phase currents by assuming all current produced from controller
+%is IQ
+%{
+Inputs: 
+A,B,C -- phase currents (column vectors)
+
+Outputs:
+Q -- Quadrature current (column vector)
+%}
+
+for i = 1:length(A)
+    Q(i) = sqrt(2/3)*norm([A(i) B(i) C(i)]);
+end
+Q = Q';
+
+end
+
